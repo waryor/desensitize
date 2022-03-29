720ae04123f05b50fc6b778a7a3b2cab454029f7
Felix Deibert <24978665+deibertf@users.noreply.github.com>
2022-03-29 23:42:37 +0200

    improved tests and added faker

--- tests/RoutingTests.php
+++ tests/RoutingTests.php
@@ -11,16 +11,16 @@ use Waryor\Desensitize\Routing\Router;
 use Waryor\Desensitize\Validator\DesensitizedUriValidator;
 
 /**
- * @runTestsInSeparateProcesses
+ *
  */
 class RoutingTests extends TestCase
 {
-    //use WithFaker;
+    use WithFaker;
 
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        //$this->setUpFaker();
+        $this->faker = $this->makeFaker('de');
     }
 
     /** @test */
@@ -55,10 +55,10 @@ class RoutingTests extends TestCase
     /** @test */
     public function test_validator_base_path_is_set_correctly()
     {
-        $basepath = 'some-fancy-name';
-        $router = $this->setUpRouter($basepath);
+        $basePath = '/' . $this->faker->domainName();
+        $router = $this->setUpRouter($basePath);
         $route = $router->get('/dummy', function (){});
-        $this->assertEquals($basepath, $route->getUriValidator()->getBasePath());
+        $this->assertEquals($basePath, $route->getUriValidator()->getBasePath());
     }
 
     /** @test */
@@ -78,7 +78,6 @@ class RoutingTests extends TestCase
     /** @test */
     public function test_routes_with_parameters()
     {
-        $this->markTestSkipped('must be revisited.');
         $router = $this->setUpRouter();
         $route = $router->get('/controller/{id}', 'TestController@with_id');
         $request = Request::create('/cOnTrOlLeR/10', 'GET');
@@ -106,7 +105,6 @@ class RoutingTests extends TestCase
     /** @test */
     public function test_routes_with_subfolder_and_parameters()
     {
-        $this->markTestSkipped('must be revisited.');
         $router = $this->setUpRouter('/folder-name');
 
         $route = $router->get('/some-controller/{id}', 'TestController@with_id');
@@ -118,6 +116,22 @@ class RoutingTests extends TestCase
         $this->assertTrue($route->matches($request), "route $route->uri did not match regex: $regex");
 
         $this->assertNotEmpty($route->parameters, 'route has no parameters but should have at least one');
+    }
+
+    /** @test */
+    public function test_routes_with_subfolder_and_parameters_and_query_string()
+    {
+        $router = $this->setUpRouter('/folder-name');
+
+        $route = $router->get('/some-controller/{id}', 'TestController@with_id');
+        $request = Request::create('/folder-name/some-controller/44?action=build&Token=2375a6aADAS', 'GET');
+
+        $route->bind($request);
+        $regex = $route->getCompiled()->getRegex();
+
+        $this->assertTrue($route->matches($request), "route $route->uri did not match regex: $regex");
+        $this->assertNotEmpty($route->parameters, 'route has no parameters but should have at least one');
+        $this->assertCount(2, $request->query, 'expected 2 items in the query string');
     }
 
     private function setUpRouter($basePath = ''): Router
